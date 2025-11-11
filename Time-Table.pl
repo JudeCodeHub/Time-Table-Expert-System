cc67c27dddb8ab7d7c65ec0e2dd35837b83506fd
JaskyNawfar <jaskinaufar@gmail.com>
2025-11-11 09:27:55 +0530

    Add dynamic predicates and course data
    Added dynamic predicates and course data to the timetable.

--- Time-Table.pl
+++ Time-Table.pl
@@ -1,1 +1,15 @@
-edit the files here
+% Dynamic Predicate
+:- dynamic schedule/3.
+
+% Course Data
+course(cs3121, 'Logic Programming and Expert System', theory).
+course(cs3111, 'Practical work on CS3121', practical).
+course(cs3122, 'Advanced Database Management Systems', theory).
+course(cs3112, 'Practical work on CS3122', practical).
+course(cs3123, 'Systems & Network Administration', theory).
+course(cs3113, 'Practical work on CS3123', practical).
+course(cs3124, 'Data Security', theory).
+course(cs3114, 'Practical work on CS3124', practical).
+course(cs3135, 'Theory of Computing', theory).
+course(eu3101, 'Foundation of Management', theory).
+course(ssi, 'Staff-Student interaction', meeting). % 'ssi' is the short ID
